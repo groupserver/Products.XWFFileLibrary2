93bbd447f02f5a32b911371da4c27709b458fa5d
richard@onlinegroups.net <richard@onlinegroups.net>
2012-07-20 21:04:48 +1200

    New file fingerprinting mechanism

---
+++ Products/XWFFileLibrary2/fingerprint.py
@@ -0,0 +1,65 @@
+import hashlib
+import string
+import time
+
+def convert_int2b(num, alphabet, converted=[]):
+    mod = num % len(alphabet); rem = num / len(alphabet)
+    converted.append(alphabet[mod])
+    if rem:
+        return convert_int2b(rem, alphabet, converted)
+    converted.reverse()
+    retval = ''.join(converted)
+    return retval
+
+def convert_int2b62(num):
+    alphabet = string.printable[:62]
+    retval = convert_int2b(num, alphabet, [])
+    return retval
+
+def fingerprint_file(file_object):
+   """ Our file fingerprint consists of three parts:
+
+         fingerprint = b62(ripe160(data)+'-'+b62(size)+'-'+b62(time)
+
+       That is, we convert the ripe160 and size to a base62 (using printable
+       ascii characters), before appending the size to the digest.
+
+   """
+   pos = file_object.tell()
+   if pos != 0:
+       file_object.seek(0)
+
+   digest = hashlib.new('ripemd160')
+
+   # iterate through in 2MB blocks
+   bs = 2**21
+   data = file_object.read(bs)
+   while data:
+       digest.update(data)
+       data = file_object.read(bs)
+
+   size = file_object.tell() 
+
+   # reset to original position
+   file_object.seek(pos)
+   
+   fingerprint = convert_int2b62(long(digest.hexdigest(), 16))+'-'+convert_int2b62(size)+'-'+convert_int2b62(int(time.time()*100))
+
+   return fingerprint
+
+def fingerprint_data(data):
+   """ Our data fingerprint consists of two parts:
+
+         fingerprint = b62(ripe160(data)+'-'+b62(size)
+
+       That is, we convert the ripe160 and size to a base62 (using printable
+       ascii characters), before appending the size to the digest.
+
+   """
+   digest = hashlib.new('ripemd160')
+   digest.update(data)
+   size = len(data)
+
+   fingerprint = convert_int2b62(long(digest.hexdigest(), 16))+'-'+convert_int2b62(size)+'-'+convert_int2b62(int(time.time()*100))
+
+   return fingerprint
