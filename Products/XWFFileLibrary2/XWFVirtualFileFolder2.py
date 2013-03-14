0af9a21bb5255b5b5b1c80b05407110242cbb4de
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-15 10:43:09 +1100

    Code cleanup of XWFVirtualFileFolder2, helped by Ninja IDE.

--- Products/XWFFileLibrary2/XWFVirtualFileFolder2.py
+++ Products/XWFFileLibrary2/XWFVirtualFileFolder2.py
@@ -18,45 +18,40 @@
 # You MUST follow the rules in http://iopen.net/STYLE before checking in code
 # to the trunk. Code which does not follow the rules will be rejected.
 #
-import types
-
-from App.class_init import InitializeClass
-
-from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from XWFFile2 import XWFFileError
-
-from DateTime import DateTime
-from AccessControl import getSecurityManager, ClassSecurityInfo
-from types import * #@UnusedWildImport
 import os
+from types import *
+from AccessControl import getSecurityManager, ClassSecurityInfo
+from App.class_init import InitializeClass
+from DateTime import DateTime  # --=mpj17=-- FIXME? datetime.datetime?
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from OFS.Folder import Folder
-from Products.XWFCore.XWFUtils import removePathsFromFilenames, convertTextToAscii
-from Products.XWFCore.XWFUtils import convertTextToId, getNotificationTemplate
-from gs.image import GSImage
-
 from zExceptions import Unauthorized
-from zope.app.file.image import Image, getImageInfo
-
+from zope.app.file.image import getImageInfo
 from zope.interface import implements
 from zope.publisher.browser import BrowserView
 from zope.app.file.browser.file import FileView
-
-from interfaces import IXWFVirtualFileFolder
-from queries import FileQuery
-from error import Hidden
 from zope.component import getMultiAdapter
 from zope.publisher.interfaces import NotFound
+from Products.XWFCore.XWFUtils import convertTextToAscii
+from Products.XWFCore.XWFUtils import convertTextToId
+from gs.image import GSImage
+from error import Hidden
+from interfaces import IXWFVirtualFileFolder
+from queries import FileQuery
 
 import logging
 log = logging.getLogger('XWFFileLibrary2.XWFVirtualFileFolder2')
 
 _marker = []
 
+
 class DisplayFile(FileView, BrowserView):
     pass
 
+
 class XWFVirtualFileFolderError(Exception):
     pass
+
 
 class XWFVirtualFileFolder2(Folder):
     """ A folder for virtualizing file library content.
@@ -67,7 +62,7 @@ class XWFVirtualFileFolder2(Folder):
         inside itself.
 
     """
-    implements( IXWFVirtualFileFolder )
+    implements(IXWFVirtualFileFolder)
     security = ClassSecurityInfo()
 
     meta_type = 'XWF Virtual Folder 2'
@@ -86,13 +81,13 @@ class XWFVirtualFileFolder2(Folder):
 
     default_nsprefix = 'file'
 
-    _properties=(
-        {'id':'title', 'type':'string', 'mode':'w'},
-        {'id':'id_factory', 'type':'string', 'mode':'w'},
-        {'id':'xwf_file_library_path', 'type':'string', 'mode':'w'},
-        {'id':'ucid', 'type':'string', 'mode':'w'},
-        {'id':'public_access_period', 'type':'int', 'mode':'w'}
-        )
+    _properties = (
+        {'id': 'title', 'type': 'string', 'mode': 'w'},
+        {'id': 'id_factory', 'type': 'string', 'mode': 'w'},
+        {'id': 'xwf_file_library_path', 'type': 'string', 'mode': 'w'},
+        {'id': 'ucid', 'type': 'string', 'mode': 'w'},
+        {'id': 'public_access_period', 'type': 'int', 'mode': 'w'}
+       )
 
     def __init__(self, id, title=None):
         """ Initialise a new instance of XWFVirtualFileFolder.
@@ -133,7 +128,7 @@ class XWFVirtualFileFolder2(Folder):
         model, submission = submit.split('+')
         model = form.get('model_override', model)
 
-        cb = getattr(self, 'cb_%s_%s' % (model,submission))
+        cb = getattr(self, 'cb_%s_%s' % (model, submission))
 
         return cb(form)
 
@@ -146,6 +141,7 @@ class XWFVirtualFileFolder2(Folder):
         return library
 
     security.declareProtected('View', 'find_files')
+
     def find_files(self, query={}):
         """ Perform a search against the files associated with this
             VirtualFileFolder.
@@ -181,6 +177,7 @@ class XWFVirtualFileFolder2(Folder):
         return retval
 
     security.declarePublic('get_file')
+
     def get_file(self, REQUEST=None, RESPONSE=None, data_only=False):
         """ """
         if REQUEST.get('REQUEST_METHOD') == 'OPTIONS':
@@ -251,14 +248,15 @@ class XWFVirtualFileFolder2(Folder):
 
     def get_xsendfile_header(self, REQUEST, RESPONSE):
         sendfile_header = None
-        if REQUEST.has_key('X-Sendfile-Type'):
+        if 'X-Sendfile-Type' in REQUEST:
             sendfile_header = REQUEST.get('X-Sendfile-Type')
-        elif REQUEST.has_key('HTTP_X_SENDFILE_TYPE'):
+        elif 'HTTP_X_SENDFILE_TYPE' in REQUEST:
             sendfile_header = REQUEST.get('HTTP_X_SENDFILE_TYPE')
 
         return sendfile_header
 
     security.declarePublic('f')
+
     def f(self, REQUEST, RESPONSE):
         """ A really short name for a file, enabling fetching a file like:
 
@@ -335,7 +333,7 @@ class XWFVirtualFileFolder2(Folder):
                     else:
                         data = self.get_file(REQUEST, RESPONSE)
                 else:
-                    img = GSImage(data).get_resized(width,height)
+                    img = GSImage(data).get_resized(width, height)
                     if img:
                         data = DisplayFile(img, REQUEST).show()
                     else:
@@ -343,41 +341,44 @@ class XWFVirtualFileFolder2(Folder):
         return data
 
     security.declareProtected('View', 'hide_file')
-    def hide_file(self, id):
-        """ """
-        object = self.find_files({'id': id})[0].getObject()
 
-        tags = list(object.getProperty('tags', []))
+    def hide_file(self, fileId):
+        """ """
+        obj = self.find_files({'id': fileId})[0].getObject()
+
+        tags = list(obj .getProperty('tags', []))
         if 'hidden' not in tags:
             tags.append('hidden')
 
-        object.manage_changeProperties(tags=tags)
-        object.reindex_object()
+        obj .manage_changeProperties(tags=tags)
+        obj .reindex_object()
 
-        return 'Hidden %s' % id
+        return 'Hidden %s' % fileId
 
     security.declareProtected('View', 'show_file')
-    def show_file(self, id):
-        """ """
-        object = self.find_files({'id': id})[0].getObject()
 
-        tags = list(object.getProperty('tags', []))
+    def show_file(self, fileId):
+        """ """
+        obj = self.find_files({'id': fileId})[0].getObject()
+
+        tags = list(obj .getProperty('tags', []))
         if 'hidden' in tags:
             tags.remove('hidden')
 
-        object.manage_changeProperties(tags=tags)
-        object.reindex_object()
+        obj .manage_changeProperties(tags=tags)
+        obj .reindex_object()
 
-        return 'Shown %s' % id
+        return 'Shown %s' % fileId
 
     security.declarePublic('OPTIONS')
+
     def OPTIONS(self, REQUEST, RESPONSE):
         """ Retrieve communication options.
 
             We disable the publishing of webdav methods.
         """
         self.dav__init(REQUEST, RESPONSE)
-        RESPONSE.setHeader('Allow', ', '.join(('GET','HEAD','POST')))
+        RESPONSE.setHeader('Allow', ', '.join(('GET', 'HEAD', 'POST')))
         #RESPONSE.setHeader('Allow', ', '.join(self.__http_methods__))
         RESPONSE.setHeader('Content-Length', 0)
         #RESPONSE.setHeader('DAV', '1,2', 1)
@@ -385,12 +386,13 @@ class XWFVirtualFileFolder2(Folder):
         return ''
 
     security.declarePublic('PROPFIND')
+
     def PROPFIND(self, REQUEST, RESPONSE):
         """ We don't support webdav, at all!
 
         """
         from zExceptions import MethodNotAllowed
-        raise MethodNotAllowed, ('Method not supported for this resource.')
+        raise MethodNotAllowed('Method not supported for this resource.')
 
     def pretty_size(self, size):
         """ Given a size, return a 'prettied' variation that most users will
@@ -400,9 +402,9 @@ class XWFVirtualFileFolder2(Folder):
         if size < 5000:
             return 'tiny'
         elif size < 1000000:
-            return '%sKB' % (size/1024)
+            return '{0}KB'.format(size / 1024)
         else:
-            return '%sMB' % (size/(1024*1024))
+            return '{0}MB'.format(size / (1024 * 1024))
 
     def printable_summary(self, result):
         """ Given a result, determine if the summary is actually printable or
@@ -419,23 +421,25 @@ manage_addXWFVirtualFileFolder2Form = PageTemplateFile(
     'management/manage_addXWFVirtualFileFolderForm.zpt',
     globals(), __name__='manage_addXWFVirtualFileFolder2Form')
 
-def manage_addXWFVirtualFileFolder2(self, id, title='',
+
+def manage_addXWFVirtualFileFolder2(self, folderId, title='',
                                REQUEST=None, RESPONSE=None, submit=None):
     """ Add a new instance of XWFVirtualFileFolder
 
     """
-    id = convertTextToId(id)
-    title = convertTextToAscii(title) or convertTextToAscii(id)
-    obj = XWFVirtualFileFolder2(id, title)
-    self._setObject(id, obj)
+    folderId = convertTextToId(folderId)
+    title = convertTextToAscii(title) or convertTextToAscii(folderId)
+    obj = XWFVirtualFileFolder2(folderId, title)
+    self._setObject(folderId, obj)
 
-    obj = getattr(self, id)
+    obj = getattr(self, folderId)
 
     if RESPONSE and submit:
         if submit.strip().lower() == 'add':
             RESPONSE.redirect('%s/manage_main' % self.DestinationURL())
         else:
-            RESPONSE.redirect('%s/manage_main' % id)
+            RESPONSE.redirect('%s/manage_main' % folderId)
+
 
 def initialize(context):
     context.registerClass(
@@ -443,5 +447,4 @@ def initialize(context):
         permission='Add XWF Virtual Folder 2',
         constructors=(manage_addXWFVirtualFileFolder2Form,
                       manage_addXWFVirtualFileFolder2),
-        icon='icons/ic-virtualfolder.png'
-        )
+        icon='icons/ic-virtualfolder.png')
