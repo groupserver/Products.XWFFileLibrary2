16c14e5e15683364eb1448d5e572b175de1859ae
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-09 15:31:10 -0700

    Updating the product metadata.

--- setup.py
+++ setup.py
@@ -12,16 +12,23 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-from setuptools import setup, find_packages
+import codecs
 import os
-
+import sys
+from setuptools import setup, find_packages
 from version import get_version
 
+version = get_version()
+
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='Products.XWFFileLibrary2',
-      version=get_version(),
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      version=version,
+      description="File (attachment) handling in GroupServer.",
+      long_description=long_description,
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -36,7 +43,9 @@ setup(name='Products.XWFFileLibrary2',
       keywords='',
       author='Richard Waid',
       author_email='richard@iopen.net',
-      url='http://groupserver.org',
+      maintainer='Michael JasonSmith',
+      maintainer_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/Products.XWFFileLibrary2/',
       license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
@@ -44,6 +53,7 @@ setup(name='Products.XWFFileLibrary2',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'gs.core',
           'gs.database',
           'gs.errormesg',
           'gs.group.messages.post',
