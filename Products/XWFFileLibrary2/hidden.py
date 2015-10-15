0b2158c0dfaf486274a049d43ea23042ebcddba7
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-15 13:40:24 -0700

    Updating the hidden file page, following the changes to gs.group.messages.post.base

--- Products/XWFFileLibrary2/hidden.py
+++ Products/XWFFileLibrary2/hidden.py
@@ -1,30 +1,38 @@
-# coding=utf-8
-from urlparse import urlparse
-from urllib import quote
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2012, 2013, 2014, 2015 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, unicode_literals, print_function
+from gs.core import mailto
 from zope.cachedescriptors.property import Lazy
-from zope.component import createObject
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from gs.errormesg.baseerror import BaseErrorPage
-from gs.group.messages.post.hiddendetails import HiddenPostInfo
-from queries import FileQuery
+from gs.group.messages.post.hide.hiddendetails import HiddenPostInfo
+from .queries import FileQuery
+
 
 class FileHidden(BaseErrorPage):
-    index = ZopeTwoPageTemplateFile('browser/templates/posthidden.pt')
+    index = ZopeTwoPageTemplateFile(b'browser/templates/posthidden.pt')
+
     def __init__(self, context, request):
         BaseErrorPage.__init__(self, context, request)
         self.requested = request.form.get('q', '')
         self.fileId = request.form.get('f', '')
         assert self.fileId, 'File ID not set on the file hidden page'
 
-    def quote(self, msg):
-        assert msg
-        retval = quote(msg)
-        assert retval
-        return retval
-
     def __call__(self, *args, **kw):
-        contentType = 'text/html; charset=UTF-8'
-        self.request.response.setHeader('Content-Type', contentType)
+        contentType = b'text/html; charset=UTF-8'
+        self.request.response.setHeader(b'Content-Type', contentType)
         self.request.response.setStatus(410)
         return self.index(self, *args, **kw)
 
@@ -40,3 +48,15 @@ class FileHidden(BaseErrorPage):
         assert retval
         return retval
 
+    @Lazy
+    def email(self):
+        subject = 'Hidden file'
+        b = '''Hello,
+
+I wanted to see the file
+  <{0}>.
+However, it is hidden. I think I should be allowed to see the file
+because...'''
+        body = b.format(self.requested)
+        retval = mailto(self.siteInfo.get_support_email(), subject, body)
+        return retval
